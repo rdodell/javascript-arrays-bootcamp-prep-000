e1e67a948cd5fb1341287da43ea5550d958c1731
Rachel Dodell <rachel@racheldodell.com>
2018-06-22 22:28:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 
-function addElementToEndOfArray(array, element){
+function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
 }
