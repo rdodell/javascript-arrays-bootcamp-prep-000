65268e8900d44fa1af7c3d087f808e6a1779ceb0
Rachel Dodell <rachel@racheldodell.com>
2018-06-22 22:31:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,30 @@ function addElementToEndOfArray(array, element) {
   array = [...array, element]
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
++  array.shift()
++  return array
++}
++
++function removeElementFromBeginningOfArray(array) {
++  return array.slice(1)
++}
++
++function destructivelyRemoveElementFromEndOfArray(array) {
++  array.pop()
++  return array
++}
++
++function removeElementFromEndOfArray(array) {
++  return array.slice(0, array.length - 1)
++}
